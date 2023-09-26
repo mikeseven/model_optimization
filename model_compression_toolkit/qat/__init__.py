2b348cbe2ba69e2547631cef14cb3c7575771c51
Mikael Bourges-Sevenier <mikael.sevenier@sima.ai>
2023-09-25 18:32:20 -0700

    fixing pytorch/keras headers

--- model_compression_toolkit/qat/__init__.py
+++ model_compression_toolkit/qat/__init__.py
@@ -13,6 +13,10 @@
 # limitations under the License.
 # ==============================================================================
 from model_compression_toolkit.qat.common.qat_config import QATConfig, TrainingMethod
+from model_compression_toolkit.constants import FOUND_TF, FOUND_TORCH
 
-from model_compression_toolkit.qat.keras.quantization_facade import keras_quantization_aware_training_init, keras_quantization_aware_training_finalize
-from model_compression_toolkit.qat.pytorch.quantization_facade import pytorch_quantization_aware_training_init, pytorch_quantization_aware_training_finalize
+if FOUND_TF:
+    from model_compression_toolkit.qat.keras.quantization_facade import keras_quantization_aware_training_init, keras_quantization_aware_training_finalize
+
+if FOUND_TORCH:
+    from model_compression_toolkit.qat.pytorch.quantization_facade import pytorch_quantization_aware_training_init, pytorch_quantization_aware_training_finalize
